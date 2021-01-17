20e960576226d464bc80d86d62214561743d1fab
romel diaz <root.romel@gmail.com>
2021-01-17 12:52:43 -0500

    init

---
+++ _nuxt/169be0e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{327:function(n,e,t){"use strict";t.r(e);var r={},c=t(35),component=Object(c.a)(r,(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("\n    servicios software\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
---
+++ _nuxt/a145f30.js
@@ -0,0 +1,2 @@
+/*! For license information please see LICENSES */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{323:function(e,n,r){"use strict";r.r(n);var o=r(298);r.d(n,"default",(function(){return o.a}));o.a.registerVersion("firebase","8.2.3","app")}}]);
